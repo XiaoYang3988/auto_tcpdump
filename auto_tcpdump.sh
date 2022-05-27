ed183dbfb0dfbe1cb9a3ca50b6544eba2f50f555
XiaoYang3988 <1807729878@qq.com>
2022-05-27 16:49:12 +0800

    Update auto_tcpdump.sh

--- auto_tcpdump.sh
+++ auto_tcpdump.sh
@@ -1,1 +1,13 @@
-echo "hello world"
+mount -o rw,remount -t auto /
+if [ ! -f "/sbin/tcpdump" ]
+then
+	cd sbin
+	curl -OJLk https://www.androidtcpdump.com/download/4.99.1.1.10.1/tcpdump
+	if [$? != 0]
+	then
+		echo "sorry download failed" 
+		exit 1
+	fi
+	chmod 777 tcpdump
+fi
+sh ./sbin/tcpdump --help
